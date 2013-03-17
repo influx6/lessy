ee4cc0dc9ccdd87aa906d72b7828dcb449fac2ad
Ewetumo Alexander <trinoxf@gmail.com>
2013-03-17 23:40:24 +0100

    adding logger possiblity

--- tests/example.js
+++ tests/example.js
@@ -1,3 +1,5 @@
 var b = require('lessy')('./css','./less');
 b.syncDir('./css');
+b.sync('ball.less'); // just add name of file directly ,path will be resolved with the import path
+b.sync('./assets/less/beach.less'); //add the relative directory,path will be resolved with root of dir where its called 
 b.bootup();
